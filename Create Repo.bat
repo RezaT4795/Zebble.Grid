8ff6512f4f1533286dea0dbeed133ec671515794
Paymon <paymon@geeks.ltd.uk>
2017-08-15 16:21:26 +0100

    Reference update

---
+++ Create Repo.bat
@@ -0,0 +1,10 @@
+
+Step 1: Add the files and compile the package.
+Step 2: Create the repo in GitHub
+Step 3: Delete these three lines and run this file, then delete this file.
+
+git init
+git remote add origin https://github.com/Geeksltd/Zebble.Grid.git
+git add -A
+git commit -m "Initial commit"
+git push
